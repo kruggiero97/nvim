82a49a94df95a3210b1646141aab5b1e9993508e
K Ruggiero <49291963+kruggiero97@users.noreply.github.com>
2026-03-27 20:31:21 -0400

    update nvim config

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'catgoose/nvim-colorizer.lua',
+    opts = {
+      user_default_options = {
+        mode = 'background',
+        names = false,
+        rgb_fn = true,
+        css = true,
+        RRGGBBAA = true,
+        AARRGGBB = true,
+      },
+    },
+  },
+}
